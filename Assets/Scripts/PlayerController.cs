b664a50eccecbd17c09661eea7eb97d27dbdf2d8
Elliott Rogers <elliott.e.w.rogers@gmail.com>
2023-10-11 11:07:33 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,78 @@
+//     void Update() 
+//     {
+
+//         Debug.Log (attackAction.triggered);
+//         Debug.Log (blockAction.ReadValue<float>());
+//     }
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController), typeof(PlayerInput))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float playerSpeed = 2.0f;
+    [SerializeField] private float jumpHeight = 1.0f;
+    [SerializeField] private float gravityValue = -9.81f;
+    
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private PlayerInput playerInput;
+    private InputAction moveAction;
+    private InputAction lookAction;
+    private InputAction jumpAction;
+    private InputAction attackAction;
+    private InputAction blockAction;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        playerInput = GetComponent<PlayerInput>();
+
+        moveAction = playerInput.actions["Move"];
+        lookAction = playerInput.actions["Look"];
+        jumpAction = playerInput.actions["Jump"];
+        attackAction = playerInput.actions["Attack"];
+        blockAction = playerInput.actions["Block"];
+    }
+
+    void Update()
+    {
+        CheckIfGrounded();
+
+        MovePlayer();
+
+        JumpPlayer();
+    }
+
+    void CheckIfGrounded()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+    }
+
+    void MovePlayer()
+    {
+        Vector2 input = moveAction.ReadValue<Vector2>();
+        Vector3 move = new Vector3(input.x, 0, input.y);
+        controller.Move(move * Time.deltaTime * playerSpeed);
+    }
+
+    void JumpPlayer()
+    {
+        // Changes the height position of the player.
+        if (jumpAction.triggered && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
