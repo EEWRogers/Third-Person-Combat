84e3bbbf6b07b811a03bd2290e7d834da05189fc
Elliott Rogers <elliott.e.w.rogers@gmail.com>
2023-11-20 15:31:25 +0000

    Began working on having health bar change colour depending on remaining health

--- Assets/Scripts/EnemyHealthBar.cs
+++ Assets/Scripts/EnemyHealthBar.cs
@@ -5,22 +5,32 @@ using UnityEngine.UI;
 
 public class EnemyHealthBar : MonoBehaviour
 {
-    [SerializeField] Slider slider;
-    [SerializeField] Camera mainCamera;
+    [SerializeField] float highHealthValue = 0.7f;
+    [SerializeField] float midHealthValue = 0.4f;
+    [SerializeField] Color highHealthColour = new Color();
+    [SerializeField] Color midHealthColour = new Color();
+    [SerializeField] Color lowHealthColour = new Color();
+
+    Slider slider;
+    Camera mainCamera;
 
     void Awake() 
     {
+        mainCamera = FindObjectOfType<Camera>();
         slider = GetComponent<Slider>();
     }
 
     void Update() 
     {
+        if (mainCamera == null) { return; }
         transform.parent.rotation = mainCamera.transform.rotation;
     }
 
     public void UpdateHealthBar(float currentHealth, float maxHealth)
     {
+        if (slider == null) { return; }
         slider.value = currentHealth / maxHealth;
+
     }
 
 }
