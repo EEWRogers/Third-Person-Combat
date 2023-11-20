0c8df730e109c758bcb737c515d319adbd9321ee
Elliott Rogers <elliott.e.w.rogers@gmail.com>
2023-11-20 14:44:38 +0000

    Created an enemy health bar that floats above the enemy in worldspace

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -8,14 +8,22 @@ public class EnemyHealth : MonoBehaviour
     [SerializeField] float maxHealth = 10f;
     float currentHealth;
 
+     EnemyHealthBar healthbar;
+
     void Awake()
     {
         currentHealth = maxHealth;
+        healthbar = GetComponentInChildren<EnemyHealthBar>();
+
+        healthbar.UpdateHealthBar(currentHealth, maxHealth);
     }
 
     public void TakeDamage(float damageAmount)
     {
         currentHealth -= damageAmount;
+
+        healthbar.UpdateHealthBar(currentHealth, maxHealth);
+        
         Debug.Log("Ouch! My health is now " + currentHealth);
 
         if (currentHealth <= 0f)
---
+++ Assets/Scripts/EnemyHealthBar.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthBar : MonoBehaviour
+{
+    [SerializeField] Slider slider;
+    [SerializeField] Camera mainCamera;
+
+    void Awake() 
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    void Update() 
+    {
+        gameObject.transform.rotation = mainCamera.transform.rotation;
+    }
+
+    public void UpdateHealthBar(float currentHealth, float maxHealth)
+    {
+        slider.value = currentHealth / maxHealth;
+    }
+
+}
